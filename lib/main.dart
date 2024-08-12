363c87fae1f6a231cb8bde39a06a3fa59cf93f84
unaaagy <umesmgar@gmail.com>
2024-08-12 12:26:04 +0545

    Update project syntax

--- lib/main.dart
+++ lib/main.dart
@@ -50,9 +50,9 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: TextButton(
+              style: ButtonStyle(
+                  foregroundColor: WidgetStateProperty.all(Colors.green)),
               child: Text(
                 'True',
                 style: TextStyle(
@@ -69,8 +69,9 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style: ButtonStyle(
+                  foregroundColor: WidgetStateProperty.all(Colors.red)),
               child: Text(
                 'False',
                 style: TextStyle(
